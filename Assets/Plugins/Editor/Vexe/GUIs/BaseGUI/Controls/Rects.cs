b42d6a931fb691093aaf327869d77e1ae2a76c2c
Laicasaane <laicasaane@outlook.com>
2016-12-22 05:18:39 +0700

    Rename Rect to RectField

--- Assets/Plugins/Editor/Vexe/GUIs/BaseGUI/Controls/Rects.cs
+++ Assets/Plugins/Editor/Vexe/GUIs/BaseGUI/Controls/Rects.cs
@@ -4,31 +4,31 @@ namespace Vexe.Editor.GUIs
 {
 	public abstract partial class BaseGUI
 	{
-		public Rect Rect(Rect value)
+		public Rect RectField(Rect value)
 		{
-			return Rect(string.Empty, value);
+			return RectField(string.Empty, value);
 		}
 
-		public Rect Rect(string label, Rect value)
+		public Rect RectField(string label, Rect value)
 		{
-			return Rect(label, value, kMultifieldOption);
+			return RectField(label, value, kMultifieldOption);
 		}
 
-		public Rect Rect(string label, string tooltip, Rect value)
+		public Rect RectField(string label, string tooltip, Rect value)
 		{
-			return Rect(label, tooltip, value, kMultifieldOption);
+			return RectField(label, tooltip, value, kMultifieldOption);
 		}
 
-		public Rect Rect(string label, Rect value, Layout option)
+		public Rect RectField(string label, Rect value, Layout option)
 		{
-			return Rect(label, string.Empty, value, option);
+			return RectField(label, string.Empty, value, option);
 		}
 
-		public Rect Rect(string label, string tooltip, Rect value, Layout option)
+		public Rect RectField(string label, string tooltip, Rect value, Layout option)
 		{
-			return Rect(new GUIContent(label, tooltip), value, option);
+			return RectField(new GUIContent(label, tooltip), value, option);
 		}
 
-		public abstract Rect Rect(GUIContent content, Rect value, Layout option);
+		public abstract Rect RectField(GUIContent content, Rect value, Layout option);
 	}
 }
