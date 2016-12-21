d2015b2a0df537ca2731b5e680e0a2d51ec8ce48
Laicasaane <laicasaane@outlook.com>
2016-12-22 05:27:08 +0700

    Rename Float to FloatField

--- Assets/Plugins/Editor/Vexe/GUIs/BaseGUI/Controls/Floats.cs
+++ Assets/Plugins/Editor/Vexe/GUIs/BaseGUI/Controls/Floats.cs
@@ -1,34 +1,34 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace Vexe.Editor.GUIs
 {
 	public abstract partial class BaseGUI
 	{
-		public float Float(float value)
+		public float FloatField(float value)
 		{
-			return Float(string.Empty, value);
+			return FloatField(string.Empty, value);
 		}
 
-		public float Float(string label, float value)
+		public float FloatField(string label, float value)
 		{
-			return Float(label, value, null);
+			return FloatField(label, value, null);
 		}
 
-		public float Float(string label, string tooltip, float value)
+		public float FloatField(string label, string tooltip, float value)
 		{
-			return Float(label, tooltip, value, null);
+			return FloatField(label, tooltip, value, null);
 		}
 
-		public float Float(string label, float value, Layout option)
+		public float FloatField(string label, float value, Layout option)
 		{
-			return Float(label, string.Empty, value, option);
+			return FloatField(label, string.Empty, value, option);
 		}
 
-		public float Float(string label, string tooltip, float value, Layout option)
+		public float FloatField(string label, string tooltip, float value, Layout option)
 		{
-			return Float(GetContent(label, tooltip), value, option);
+			return FloatField(GetContent(label, tooltip), value, option);
 		}
 
-		public abstract float Float(GUIContent content, float value, Layout option);
+		public abstract float FloatField(GUIContent content, float value, Layout option);
 	}
 }
