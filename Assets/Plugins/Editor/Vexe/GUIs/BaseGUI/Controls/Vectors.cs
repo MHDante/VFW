78e901ad42ce544cc82779779920c115e0d94462
Laicasaane <laicasaane@outlook.com>
2016-12-22 05:17:06 +0700

    Improve: rename Vector2 and Vector3 to Vector2Field and Vector3Field, add Vector4Field

--- Assets/Plugins/Editor/Vexe/GUIs/BaseGUI/Controls/Vectors.cs
+++ Assets/Plugins/Editor/Vexe/GUIs/BaseGUI/Controls/Vectors.cs
@@ -4,84 +4,196 @@ namespace Vexe.Editor.GUIs
 {
 	public abstract partial class BaseGUI
 	{
-		public Vector3 Vector3(Vector3 value)
+		public Vector4 Vector4Field(Vector4 value)
 		{
-			return Vector3(string.Empty, value);
+			return Vector4Field(string.Empty, value);
 		}
 
-		public Vector3 Vector3(string label, Vector3 value)
+		public Vector4 Vector4Field(string label, Vector4 value)
 		{
-			return Vector3(label, value, null);
+			return Vector4Field(label, value, null);
 		}
 
-		public Vector3 Vector3(string label, string tooltip, Vector3 value)
+		public Vector4 Vector4Field(string label, string tooltip, Vector4 value)
 		{
-			return Vector3(label, tooltip, value, null);
+			return Vector4Field(label, tooltip, value, null);
 		}
 
-		public Vector3 Vector3(string label, Vector3 value, Layout option)
+		public Vector4 Vector4Field(string label, Vector4 value, Layout option)
 		{
-			return Vector3(label, string.Empty, value, option);
+			return Vector4Field(label, string.Empty, value, option);
 		}
 
-		public Vector3 Vector3(string label, string tooltip, Vector3 value, Layout option)
+		public Vector4 Vector4Field(string label, string tooltip, Vector4 value, Layout option)
 		{
-			return Vector3(GetContent(label, tooltip), value, option);
+			return Vector4Field(GetContent(label, tooltip), value, option);
 		}
 
-		public Vector3 Vector3(GUIContent content, Vector3 value, Layout option)
+		public Vector4 Vector4Field(GUIContent content, Vector4 value, Layout option)
 		{
-			using (Horizontal())
+			if (hasReachedMinScreenWidth &&
+				content != null && !string.IsNullOrEmpty(content.text))
 			{
-				Prefix(content.text);
-				using (LabelWidth(13f))
+				using (Vertical())
 				{
-					value.x = Float("X", value.x);
-					value.y = Float("Y", value.y);
-					value.z = Float("Z", value.z);
+					Prefix(content);
+					using (Horizontal())
+					{
+						Space(15f);
+						INTERNAL_Vector4(ref value);
+					}
+				}
+			}
+			else
+			{
+				using (Horizontal())
+				{
+					Prefix(content);
+					INTERNAL_Vector4(ref value);
 				}
 			}
 
 			return value;
 		}
 
-		public Vector2 Vector2(Vector2 value)
+		private void INTERNAL_Vector4(ref Vector4 value)
 		{
-			return Vector2(string.Empty, value);
+			using (LabelWidth(15f))
+			{
+				value.x = FloatField("X", value.x);
+				Space(2f);
+				value.y = FloatField("Y", value.y);
+				Space(2f);
+				value.z = FloatField("Z", value.z);
+				Space(2f);
+				value.w = FloatField("W", value.w);
+			}
 		}
 
-		public Vector2 Vector2(string label, Vector2 value)
+		public Vector3 Vector3Field(Vector3 value)
 		{
-			return Vector2(label, value, null);
+			return Vector3Field(string.Empty, value);
+		}
+
+		public Vector3 Vector3Field(string label, Vector3 value)
+		{
+			return Vector3Field(label, value, null);
+		}
+
+		public Vector3 Vector3Field(string label, string tooltip, Vector3 value)
+		{
+			return Vector3Field(label, tooltip, value, null);
+		}
+
+		public Vector3 Vector3Field(string label, Vector3 value, Layout option)
+		{
+			return Vector3Field(label, string.Empty, value, option);
+		}
+
+		public Vector3 Vector3Field(string label, string tooltip, Vector3 value, Layout option)
+		{
+			return Vector3Field(GetContent(label, tooltip), value, option);
+		}
+
+		public Vector3 Vector3Field(GUIContent content, Vector3 value, Layout option)
+		{
+			if (hasReachedMinScreenWidth &&
+				content != null && !string.IsNullOrEmpty(content.text))
+			{
+				using (Vertical())
+				{
+					Prefix(content);
+					using (Horizontal())
+					{
+						Space(15f);
+						INTERNAL_Vector3(ref value);
+					}
+				}
+			}
+			else
+			{
+				using (Horizontal())
+				{
+					Prefix(content);
+					INTERNAL_Vector3(ref value);
+				}
+			}
+
+			return value;
+		}
+
+		private void INTERNAL_Vector3(ref Vector3 value)
+		{
+			using (LabelWidth(15f))
+			{
+				value.x = FloatField("X", value.x);
+				Space(2f);
+				value.y = FloatField("Y", value.y);
+				Space(2f);
+				value.z = FloatField("Z", value.z);
+			}
+		}
+
+		public Vector2 Vector2Field(Vector2 value)
+		{
+			return Vector2Field(string.Empty, value);
+		}
+
+		public Vector2 Vector2Field(string label, Vector2 value)
+		{
+			return Vector2Field(label, value, null);
 		}
 
 		public Vector2 Vector2Field(string label, string tooltip, Vector2 value)
 		{
-			return Vector2(label, tooltip, value, null);
+			return Vector2Field(label, tooltip, value, null);
 		}
 
-		public Vector2 Vector2(string label, Vector2 value, Layout option)
+		public Vector2 Vector2Field(string label, Vector2 value, Layout option)
 		{
-			return Vector2(label, string.Empty, value, option);
+			return Vector2Field(label, string.Empty, value, option);
 		}
 
-		public Vector2 Vector2(string label, string tooltip, Vector2 value, Layout option)
+		public Vector2 Vector2Field(string label, string tooltip, Vector2 value, Layout option)
 		{
-			return Vector2(GetContent(label, tooltip), value, option);
+			return Vector2Field(GetContent(label, tooltip), value, option);
 		}
 
-		public Vector2 Vector2(GUIContent content, Vector2 value, Layout option)
+		public Vector2 Vector2Field(GUIContent content, Vector2 value, Layout option)
 		{
-			using (Horizontal())
+			if (hasReachedMinScreenWidth &&
+				content != null && !string.IsNullOrEmpty(content.text))
 			{
-				Prefix(content.text);
-				using (LabelWidth(13f))
+				using (Vertical())
 				{
-					value.x = Float("X", value.x);
-					value.y = Float("Y", value.y);
+					Prefix(content);
+					using (Horizontal())
+					{
+						Space(15f);
+						INTERNAL_Vector2(ref value);
+					}
 				}
 			}
+			else
+			{
+				using (Horizontal())
+				{
+					Prefix(content);
+					INTERNAL_Vector2(ref value);
+				}
+			}
+
 			return value;
+		}
+
+		private void INTERNAL_Vector2(ref Vector2 value)
+		{
+			using (LabelWidth(15f))
+			{
+				value.x = FloatField("X", value.x);
+				Space(2f);
+				value.y = FloatField("Y", value.y);
+			}
 		}
 	}
 }
